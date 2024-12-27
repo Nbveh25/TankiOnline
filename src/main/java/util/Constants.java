83c7374d94d9aeeb53cac876861b80a20066f8bf
Timur Bikmukhametov <tima.bikmukhametov@inbox.ru>
2024-12-28 01:40:07 +0300

    Constants added, smoki resources completed + smoki in different directions added + ConstantsUtil added

---
+++ src/main/java/util/Constants.java
@@ -0,0 +1,12 @@
+package util;
+
+public class Constants {
+    // APP NAME
+    public static final String TANKI_ONLINE = "Tanki Online";
+
+    // DIRECTIONS
+    public static final String UP = "up";
+    public static final String DOWN = "down";
+    public static final String LEFT = "left";
+    public static final String RIGHT = "right";
+}
